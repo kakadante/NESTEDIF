1e8a373ff8f41c8505c0b0b447924f3a66ec1f9f
kaka_dante <danielmasila76@gmail.com>
2020-06-19 14:49:01 +0300

    kaka

---
+++ NESTEDIF/Pages/CallCenter.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NESTEDIF
+{
+    public class CallCenterModel : PageModel
+    {
+        public CallCenterModel()
+        {
+
+        }
+        public void OnGet()
+        {
+
+                //var Q1 = "Hello, Good Morning/afternoon/evening. My Name is Cynthia Calling you from ABC, could I speak to Mr./Mrs/Ms./Dr./Prof. ";
+                //Console.WriteLine(Q1);
+
+        }
+
+        //public IActionResult OnPost()
+        //{
+
+        //    var Q1 = "Hello, Good Morning/afternoon/evening. My Name is Cynthia Calling you from ABC, could I speak to Mr./Mrs/Ms./Dr./Prof. ";
+        //    return Q1();
+        //}
+
+    }
+}
