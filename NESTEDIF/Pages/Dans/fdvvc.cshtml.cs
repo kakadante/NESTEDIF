152f6b937c59afdaf45803d44d6c425bfd1d7c01
kaka_dante <danielmasila76@gmail.com>
2020-06-20 06:31:01 +0300

    kaseee...hehehe...manu

---
+++ NESTEDIF/Pages/Dans/fdvvc.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NESTEDIF
+{
+    public class fdvvcModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
